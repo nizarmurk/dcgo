9173e2473fce2da8162ad1da49051c4eefffcf4f
Nizar Murk <nizarmurk98@gmail.com>
2023-05-09 00:10:45 +0200

    removed env.BOTNAME

--- auth/discordauth.go
+++ auth/discordauth.go
@@ -2,15 +2,27 @@ package auth
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/nizarmurk/dcgo/env"
 )
 
 func DiscordAuth() {
-	_, err := discordgo.New(env.BOTNAME + env.TOKEN)
+	session, err := discordgo.New(env.TOKEN)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	session.Open()
+
+	// Signal-Handler hinzufügen
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	// Auf Signale warten
+	<-sigChan
+
 }
